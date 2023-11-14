e33899aa3ad8539c4298d35986461e4e1d0ab32f
unknown <2116002@student.ncs.ac.jp>
2023-11-14 15:44:39 +0900

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { getTrends } from "@/art/getTrends"
-import { AddArtButton } from "./AddArtButton"
 
 export default async function Home() {
-  const trends = await getTrends()
   return (
     <div>
-      {trends.map(art =>
-        <div key={art.id}>
-          {art.title}
-        </div>
-      )}
-      <AddArtButton />
     </div>
   )
 }
---
+++ src/app/react-practice/test/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+
+/* 
+はじめは「ほりお」と表示される
+ボタンを用意し、ボタンをクリックすると「ほりお」から「うちやま」にかわる
+再度ボタンをクリックすると「うちやま」から「ほりお」にかわる
+*/
+
+const TestPage = ({ }: {}) => {
+    return (
+        <div>
+            <button>
+                ボタ
+            </button>
+        </div>
+    )
+}
+export default TestPage
