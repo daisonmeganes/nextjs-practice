3e1ae68f37042e9ee88b991625294961799f1905
unknown <2116002@student.ncs.ac.jp>
2023-11-15 16:28:50 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { getTrends } from "@/art/getTrends"
+//import { add } from "@/art/add"
 
 export default async function Home() {
+  const arts = getTrends()
   return (
     <div>
+
+      <h1>作品登録</h1>
+
+      <button className='btn btn-primary mt-3'>追加</button>
+
+      <div>
+        {(await arts).map(art =>
+          <div key={art.id}>
+            <h1>{art.id}</h1>
+            <h2>{art.title}</h2>
+            <h3>{art.author}</h3>
+            <h4>{art.tag}</h4>
+          </div>)}
+
+      </div>
+
 
     </div>
   )
